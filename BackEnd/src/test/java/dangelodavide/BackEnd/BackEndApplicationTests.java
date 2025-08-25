e539505189636c42da4b7d980b56b6d1975bab5c
DngDavide02 <dangelodavide02@gmail.com>
2025-08-25 10:14:39 +0200

    Initial commit

---
+++ BackEnd/src/test/java/dangelodavide/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package dangelodavide.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
