663b2975bcc1350b9d77891da817753809f3a27f
DngDavide02 <dangelodavide02@gmail.com>
2025-08-26 11:13:14 +0200

    exception

---
+++ BackEnd/src/main/java/dangelodavide/BackEnd/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package dangelodavide.BackEnd.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
