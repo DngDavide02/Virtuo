8e38fc0dbb36a14dd3531dc9103a7242e650e5d3
DngDavide02 <dangelodavide02@gmail.com>
2025-09-01 16:09:28 +0200

    account page discard

--- FrontEnd/src/pages/Account.jsx
+++ FrontEnd/src/pages/Account.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import ProfileInfo from "../components/ProfileInfo";
-import LibraryList from "../components/LibraryList";
-import { useLibrary } from "../js/useLibrary";
-import "../css/account.css";
 
 export default function Account() {
-  const { library, removeGame } = useLibrary();
-
   return (
     <Container className="mt-5">
       <h2>Account Page</h2>
-      <ProfileInfo />
-      <LibraryList library={library} removeGame={removeGame} />
     </Container>
   );
 }
